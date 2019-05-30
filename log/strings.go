55239c9c3a7b8c663a1cddea8d99c985c01461dc
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-05-31 00:43:48 +0200

    Time format and NowString() convenience function.

---
+++ log/strings.go
@@ -0,0 +1,18 @@
+package log
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	// timeFmt is a very detailed time string.
+	timeFmt = "%s %s %02d %02d:%02d:%02d.%06d %04d"
+)
+
+// NowString returns a very detailed time string.
+func NowString() string {
+	t := time.Now()
+	return fmt.Sprintf(timeFmt, t.Weekday().String()[0:3], t.Month().String()[0:3], t.Day(),
+		t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), t.Year())
+}
