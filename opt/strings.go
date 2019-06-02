82b51d698e0743380215243ac943700c68fec212
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-06-02 13:10:37 +0200

    Added error message for tool commands to return when desirable to output usage infomation.

--- opt/strings.go
+++ opt/strings.go
@@ -1,5 +1,8 @@
 package opt
 
 const (
+	// ErrorUsage should be returned from tool commands to show its options.
+	ErrorUsage = "unknown options"
+	// ErrorNoCommand is also used internally when something breaks.
 	ErrorNoCommand = "no command specified"
 )
