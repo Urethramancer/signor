709f2382da8a332c5523c91be397e16bde136b42
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-06-02 14:01:39 +0200

    A little renaming and some comments happened.

--- server/server.go
+++ server/server.go
@@ -11,22 +11,25 @@ type Server struct {
 	sync.RWMutex
 	sync.WaitGroup
 	// Name for logging purposes and management.
-	Name string
-	web  map[string]*Site
-	// Very lazy default logging
-	l    func(string, ...interface{})
-	e    func(string, ...interface{})
+	Name   string
+	web    map[string]*Site
+	logger *log.Logger
+	// L is the regular message output command from the Logger object.
+	L func(string, ...interface{})
+	// E us the error message output command from the Logger object.
+	E    func(string, ...interface{})
 	quit chan bool
 }
 
 // New server instance creation.
 func New(name string) *Server {
 	s := Server{
-		Name: name,
-		quit: make(chan bool, 1),
-		web:  make(map[string]*Site),
-		l:    log.Default.TMsg,
-		e:    log.Default.TErr,
+		Name:   name,
+		quit:   make(chan bool, 1),
+		web:    make(map[string]*Site),
+		logger: log.Default,
+		L:      log.Default.TMsg,
+		E:      log.Default.TErr,
 	}
 
 	return &s
@@ -34,12 +37,12 @@ func New(name string) *Server {
 
 // Start all configured sub-servers.
 func (s *Server) Start() error {
-	s.l("Starting server '%s'.", s.Name)
+	s.L("Starting server '%s'.", s.Name)
 
 	s.Add(1)
 	go func() {
 		<-s.quit
-		s.l("Quitting server '%s'.", s.Name)
+		s.L("Quitting server '%s'.", s.Name)
 		s.Done()
 	}()
 	return nil
@@ -49,4 +52,11 @@ func (s *Server) Start() error {
 func (s *Server) Stop() {
 	s.quit <- true
 	s.Wait()
+}
+
+// SetLogger changes the logger object and sets the message shortcuts for convenience.
+func (s *Server) SetLogger(l *log.Logger) {
+	s.logger = l
+	s.L = l.TMsg
+	s.E = l.TErr
 }
