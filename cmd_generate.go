caf41e856c8306d3918d4a43c4b20a6d834939e8
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-06-03 00:52:20 +0200

    Should consider an automatic option for help.

--- cmd_generate.go
+++ cmd_generate.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"errors"
+
+	"github.com/Urethramancer/signor/opt"
+)
+
 type GenerateCmd struct {
 	Travis TravisCmd `command:"travis" help:"Generate .travis.yml files for GitHub projects."`
+}
+
+func (g *GenerateCmd) Run(in []string) error {
+	return errors.New(opt.ErrorUsage)
 }
