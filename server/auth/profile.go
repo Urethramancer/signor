5c5dc552abfdac496f9e31c51012070410cb28f3
Ronny Bangsund <github@neuralshortcircuit.com>
2019-06-10 22:50:33 +0200

    Planning authentication.

---
+++ server/auth/profile.go
@@ -0,0 +1,6 @@
+package auth
+
+// Profile is used per domain.
+type Profile struct {
+	UserID int64 `json:"userid"`
+}
---
+++ server/auth/user.go
@@ -0,0 +1,16 @@
+package auth
+
+// User structure for the main system.
+type User struct {
+	// ID is a unique identifier in the database.
+	ID int64 `json:"id"`
+	// Name is the username for logins and the default display name.
+	Name string `json:"name"`
+	// Email is used for verification and password retrieval.
+	Email string `json:"email"`
+	// Fullnames is the real name of the user in western order (first name
+	// followed by middle names, ending in last names and everything else between).
+	Fullnames []string `json:"fullnames,omitempty"`
+	// Title if applicable.
+	Title string `json:"title,omitempty"`
+}
