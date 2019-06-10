71c274273186c19cfbf8ddc49417c1d069c246c2
Ronny Bangsund <github@neuralshortcircuit.com>
2019-06-10 10:54:32 +0200

    Refactored and made web servers background properly with waitgroups to synchronise.

--- server/server.go
+++ server/server.go
@@ -12,18 +12,20 @@ import (
 type Server struct {
 	sync.RWMutex
 	sync.WaitGroup
-	// Name for logging purposes and management.
-	Name string
-	web  *web.Web
 	log.LogShortcuts
-	quit chan bool
+
+	// Name for logging purposes and management.
+	Name       string
+	webservers map[string]*web.Web
+	quit       chan bool
 }
 
 // New server instance creation.
 func New(name string) *Server {
 	s := Server{
-		Name: name,
-		quit: make(chan bool, 1),
+		Name:       name,
+		webservers: make(map[string]*web.Web),
+		quit:       make(chan bool, 1),
 	}
 	s.Logger = log.Default
 	s.L = log.Default.TMsg
@@ -46,9 +48,14 @@ func (s *Server) Start() error {
 }
 
 // Stop all sub-servers.
-func (s *Server) Stop() {
+func (s *Server) Stop() error {
 	s.quit <- true
+	err := s.StopWeb()
+	if err != nil {
+		return err
+	}
 	s.Wait()
+	return nil
 }
 
 // SetLogger changes the logger object and sets the message shortcuts for convenience.
@@ -56,30 +63,55 @@ func (s *Server) SetLogger(l *log.Logger) {
 	s.Logger = l
 	s.L = l.TMsg
 	s.E = l.TErr
-	if s.web != nil {
-		s.web.SetLogger(l)
+	for _, w := range s.webservers {
+		w.SetLogger(l)
 	}
 }
 
 // AddWebServer to the server.
-func (s *Server) AddWebServer(address, secureport, otherport string) *web.Web {
-	cfg := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		NextProtos:               []string{"http/1.1"},
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_AES_128_GCM_SHA256,
-			tls.TLS_AES_256_GCM_SHA384,
-			tls.TLS_CHACHA20_POLY1305_SHA256,
-		},
+func (s *Server) AddWebServer(address, port string, secure bool) *web.Web {
+	var w *web.Web
+	if secure {
+		cfg := &tls.Config{
+			MinVersion:               tls.VersionTLS12,
+			NextProtos:               []string{"http/1.1"},
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+			},
+		}
+		w = web.New(address, port, s.Logger, cfg)
+	} else {
+		w = web.New(address, port, s.Logger, nil)
 	}
 
-	w := web.New(address, secureport, otherport, s.Logger, cfg)
-	s.web = w
+	s.webservers[port] = w
 	return w
+}
+
+// StartWeb starts all web servers.
+func (s *Server) StartWeb() {
+	for _, w := range s.webservers {
+		go w.Start()
+	}
+}
+
+// StopWeb stops all web servers.
+func (s *Server) StopWeb() error {
+	var err error
+	for _, w := range s.webservers {
+		err = w.Stop()
+		if err != nil {
+			return err
+		}
+		w.Wait()
+	}
+	return nil
 }
--- server/web/web.go
+++ server/web/web.go
@@ -6,18 +6,19 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/Urethramancer/signor/log"
 )
 
 type Web struct {
+	sync.RWMutex
+	sync.WaitGroup
 	// Address to bind the web server to.
 	Address string `json:"address"`
-	// SecurePort is optional, and will default to 443.
-	SecurePort string `json:"secureport,omitempty"`
-	// OtherPort is optional, and will default to 80.
-	OtherPort string `json:"otherport,omitempty"`
+	// Port is optional, and will default to 80 or 443, depending on presence of certificates.
+	Port string `json:"secureport,omitempty"`
 
 	//
 	// Internals
@@ -26,14 +27,25 @@ type Web struct {
 	sites       map[string]*Site
 	server      *http.Server
 	log.LogShortcuts
+	secure  bool
+	running bool
 }
 
-func New(address, secureport, otherport string, logger *log.Logger, cfg *tls.Config) *Web {
+// New creates a web server, configured with reasonable timeouts and a default handlers.
+// Use AddSite() to add handlers for domains.
+func New(address, port string, logger *log.Logger, cfg *tls.Config) *Web {
+	if port == "" {
+		if cfg == nil {
+			port = "80"
+		} else {
+			port = "443"
+		}
+	}
+
 	w := Web{
-		sites:      make(map[string]*Site),
-		Address:    address,
-		SecurePort: secureport,
-		OtherPort:  otherport,
+		sites:   make(map[string]*Site),
+		Address: address,
+		Port:    port,
 	}
 	w.Logger = logger
 	w.L = logger.TMsg
@@ -46,12 +58,18 @@ func New(address, secureport, otherport string, logger *log.Logger, cfg *tls.Con
 		TLSConfig:    cfg,
 	}
 
+	if cfg != nil {
+		w.secure = true
+	}
+
 	http.HandleFunc("/", w.defaultHandler)
 	return &w
 }
 
 // SetLogger changes the logger object and sets the message shortcuts for convenience.
 func (w *Web) SetLogger(l *log.Logger) {
+	w.Lock()
+	defer w.Unlock()
 	w.Logger = l
 	w.L = l.TMsg
 	w.E = l.TErr
@@ -59,26 +77,41 @@ func (w *Web) SetLogger(l *log.Logger) {
 
 // AddCertificate from loaded certificate.
 func (w *Web) AddCertificate(cert tls.Certificate) {
+	w.Lock()
+	defer w.Unlock()
 	w.server.TLSConfig.Certificates = append(w.server.TLSConfig.Certificates, cert)
 	w.server.TLSConfig.BuildNameToCertificate()
 }
 
 // RebuildCertificates reloads the certificates from all sites.
-func (w *Web) RebuildCertificates() {
+func (w *Web) RebuildCertificates() error {
+	w.Lock()
+	defer w.Unlock()
+	for _, s := range w.secureSites {
+		cert, err := tls.LoadX509KeyPair(s.Certificate, s.Key)
+		if err != nil {
+			return err
+		}
+
+		w.AddCertificate(cert)
+	}
 	w.server.TLSConfig.BuildNameToCertificate()
+	return nil
 }
 
 // AddSite to a web server.
 // This is done on the fly without need of any restarting.
 func (w *Web) AddSite(s *Site) error {
-	cert, err := tls.LoadX509KeyPair(s.Certificate, s.Key)
-	if err != nil {
-		return err
-	}
+	if w.secure {
+		cert, err := tls.LoadX509KeyPair(s.Certificate, s.Key)
+		if err != nil {
+			return err
+		}
 
-	//TODO: Watch key+cert for changes and automatically reload.
-	//TODO: Let's Encrypt support.
-	w.AddCertificate(cert)
+		//TODO: Watch key+cert for changes and automatically reload.
+		//TODO: Let's Encrypt support.
+		w.AddCertificate(cert)
+	}
 	w.sites[s.Domain] = s
 	http.HandleFunc(s.Domain+"/", s.DefaultHandler)
 	return nil
@@ -86,22 +119,62 @@ func (w *Web) AddSite(s *Site) error {
 
 // Start the webserver.
 func (w *Web) Start() error {
-	w.RebuildCertificates()
+	if w.running {
+		return nil
+	}
+
+	w.Lock()
+	defer w.Unlock()
+	w.running = true
+
 	var err error
-	listener, err := tls.Listen("tcp", net.JoinHostPort(w.Address, w.SecurePort), w.server.TLSConfig)
+	addr := net.JoinHostPort(w.Address, w.Port)
+	w.L("Starting web server on %s", addr)
+	if w.secure {
+		w.RebuildCertificates()
+		listener, err := tls.Listen("tcp", addr, w.server.TLSConfig)
+		if err != nil {
+			w.running = false
+			return err
+		}
+
+		w.Add(1)
+		return w.server.Serve(listener)
+	}
+
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		w.running = false
+		return err
+	}
+
+	w.Add(1)
+	return w.server.Serve(listener)
+}
+
+// Stop the webserver and try to wait till all connections are done.
+// Give up after half a second.
+func (w *Web) Stop() error {
+	w.L("Stopping web server on %s:%s", w.Address, w.Port)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*500)
+	defer cancel()
+	err := w.server.Shutdown(ctx)
+	w.Done()
 	if err != nil {
 		return err
 	}
 
-	return w.server.Serve(listener)
-}
-
-// Stop the webserver and wait till all connections are done.
-func (w *Web) Stop() error {
-	return w.server.Shutdown(context.Background())
+	w.running = false
+	return nil
 }
 
 // defaultHandler when no sites are configured on a requested URL.
-func (web *Web) defaultHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "No site configured on %s", r.Host)
+func (w *Web) defaultHandler(wr http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(wr, "No site configured on %s", r.Host)
+}
+
+// LoadSites loads sites from JSON files in the specified path (non-recursively).
+func (w *Web) LoadSites(path string) error {
+
+	return nil
 }
