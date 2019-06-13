946e955b351017d047121f42cd818b2bc1ba4df3
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-06-13 08:01:35 +0200

    Added comment.

--- gen_rest.go
+++ gen_rest.go
@@ -10,12 +10,14 @@ import (
 type RESTCmd struct {
 	// Help flag.
 	Help bool `short:"h" long:"help" help:"Show usage."`
+
 	// Input is the source file to generate from.
 	Input string `help:"Input file with structures for REST endpoints." placeholder:"INFILE"`
 	// Output is the Go file to generate.
 	Output string `help:"Output Go source file." placeholder:"OUTFILE"`
 }
 
+// Run the REST code generator.
 func (cmd *RESTCmd) Run(in []string) error {
 	if cmd.Help || cmd.Output == "" {
 		return errors.New(opt.ErrorUsage)
