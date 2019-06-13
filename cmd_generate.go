72ba2f764c1b318515ff5f5e1551ca11fac157c5
Ronny Bangsund <orb@neuralshortcircuit.com>
2019-06-13 08:00:03 +0200

    DB code generation started.

--- cmd_generate.go
+++ cmd_generate.go
@@ -10,6 +10,7 @@ type GenerateCmd struct {
 	opt.DefaultHelp
 	Travis TravisCmd `command:"travis" help:"Generate .travis.yml files for GitHub projects."`
 	REST   RESTCmd   `command:"rest" help:"Generate REST code from tagged structures."`
+	DB     DBCmd     `command:"database" help:"Generate database schema from tagged structures." aliases:"db"`
 }
 
 func (cmd *GenerateCmd) Run(in []string) error {
---
+++ gen_db.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/Urethramancer/signor/log"
+	"github.com/Urethramancer/signor/opt"
+	"github.com/Urethramancer/signor/structure"
+)
+
+type DBCmd struct {
+	// Help flag.
+	Help bool `short:"h" long:"help" help:"Show usage."`
+
+	// Package name.
+	Package string `short:"p" long:"package" help:"Package name." placeholder:"STRING" default:"database"`
+	// User table flag.
+	User bool `short:"u" long:"user" help:"Set this as a user table for authentication, and generate special code."`
+	// Type of database. Only Postgres is supported for now.
+	Type string `short:"t" long:"type" help:"Type of database." choices:"pg"`
+
+	// Input is the source file to generate from.
+	Input string `help:"Input file with structures for database tables." placeholder:"INFILE"`
+	// Output is the directory to generate output files in.
+	Output string `help:"Output directory." placeholder:"PATH"`
+}
+
+// Run the DB code generator.
+func (cmd *DBCmd) Run(in []string) error {
+	if cmd.Help || cmd.Output == "" {
+		return errors.New(opt.ErrorUsage)
+	}
+
+	pr := log.Default.Printf
+	pkg, err := structure.NewPackage(cmd.Input)
+	if err != nil {
+		return err
+	}
+
+	pkg.Name = cmd.Package
+	pr("%s", pkg.String())
+	return nil
+}
